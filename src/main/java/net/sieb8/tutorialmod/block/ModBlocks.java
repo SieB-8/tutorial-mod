be51f59cdcf70f466a42d35a28f41812a9b912d9
SieB-8 <siebe.swinnen27@gmail.com>
2024-11-21 20:51:34 +0100

    Tool requirements and loottables

--- src/main/java/net/sieb8/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/sieb8/tutorialmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.sieb8.tutorialmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.sieb8.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -21,7 +23,8 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.SCULK)));
     public static final Block ORE = registerBlock("ore",
-            new Block(AbstractBlock.Settings.create().strength(4f)
+            new ExperienceDroppingBlock(UniformIntProvider.create(100, 1000),
+                    AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.STONE)));
 
     private static Block registerBlock(String name, Block block) {
